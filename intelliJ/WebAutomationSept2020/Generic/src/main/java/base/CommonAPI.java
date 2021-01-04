4007d6784cf590e767ad7a15f71dce32000cebbc
jahmed04 <jisanx2020@gmail.com>
2021-01-04 08:59:28 -0500

    CloudSetupUpdate

--- intelliJ/WebAutomationSept2020/Generic/src/main/java/base/CommonAPI.java
+++ intelliJ/WebAutomationSept2020/Generic/src/main/java/base/CommonAPI.java
@@ -5,6 +5,11 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -12,20 +17,38 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class CommonAPI {
 
+    public String browserStackUserName = "your_user_name";
+    public String browserStackAccessKey = "Your_Access_Key";
+    public String sauceLabsUserName = "";
+    public String sauceLabsAccessKey = "";
+
+
     public static WebDriver driver = null;
 
-    @Parameters({"url"})
+    @Parameters({ "useCloudEnv", "envName",  "os",  "os_version",
+                 "browserName",  "browserVersion","url"})
     @BeforeMethod
-    public void setUp(String url) {
+    public void setUp(String useCloudEnv, String envName, String os, String os_version,
+                      String browserName, String browserVersion, String url) throws MalformedURLException {
+
+        if(useCloudEnv.equalsIgnoreCase("true")) {
+            getCloudDriver( envName,  os,  os_version,
+                     browserName,  browserVersion );
+        }
+        else if (useCloudEnv.equalsIgnoreCase("false")) {
+            getLocalDriver(os, browserName);
+        }
 
         // using firefox driver
-        //System.setProperty("webdriver.gecko.driver", "/Users/ahmed/intelliJ/SeleniumProject1/driver/geckodriver");
+        //System.setProperty("webdriver.gecko.driver", "/Users/ahmed/intelliJ/WebAutomationSept2020/Generic/driver/geckodriver");
 
         // using chrome driver
         System.setProperty("webdriver.chrome.driver", "/Users/ahmed/intelliJ/WebAutomationSept2020/Generic/driver/chromedriver");
@@ -35,15 +58,72 @@ public class CommonAPI {
 
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        //  driver.get("http://amazon.com");
-        //  driver.get("https://nypost.com/");
-
 
         // driver.get(url);  // website address (one way to get url)
         driver.navigate().to(url);
 
         driver.manage().window().maximize();  // commands to window
     }
+
+
+    // local drivers: chrome, firefox, safari, opera etc
+    public WebDriver getLocalDriver(String os, String browserName) {
+
+        if (browserName.equalsIgnoreCase("chrome")) {
+            if(os.equalsIgnoreCase("OS X")) {
+                System.setProperty("webdriver.chrome.driver", "/Users/ahmed/intelliJ/WebAutomationSept2020/Generic/driver/chromedriver");
+                driver = new ChromeDriver();
+            }
+            else if (os.equalsIgnoreCase("Windows")) {
+                System.setProperty("webdriver.chrome.driver", "/Users/ahmed/intelliJ/WebAutomationSept2020/Generic/driver/chromedriver.exe");
+                driver = new ChromeDriver();
+            }
+        }
+        else if (browserName.equalsIgnoreCase("firefox")) {
+            if(os.equalsIgnoreCase("OS X")) {
+                System.setProperty("webdriver.gecko.driver", "/Users/ahmed/intelliJ/WebAutomationSept2020/Generic/driver/geckodriver");
+                driver = new FirefoxDriver();
+            }else if (os.equalsIgnoreCase("Windows")) {
+                System.setProperty("webdriver.gecko.driver", "/Users/ahmed/intelliJ/WebAutomationSept2020/Generic/driver/geckodriver.exe");
+                driver = new FirefoxDriver();
+            }
+        }
+        else if(browserName.equalsIgnoreCase("ie")){
+            System.setProperty("webdriver.ie.driver", "./Generic/driver/internetexplorerdriver.exe");
+            driver = new InternetExplorerDriver();
+
+        }else if(browserName.equalsIgnoreCase("safari")){
+            System.setProperty("webdriver.safari.driver", "./Generic/driver/safaridriver");
+            driver = new SafariDriver();
+        }
+        return driver;
+    }
+
+
+
+    // cloud driver: browserstack or saucelabs
+
+    public WebDriver getCloudDriver(String envName, String os, String os_version, String browserName, String browserVersion) throws MalformedURLException {
+
+        DesiredCapabilities cap = new DesiredCapabilities();
+
+        cap.setCapability("broswer", browserName);
+        cap.setCapability("brwoserVersion", browserVersion);
+        cap.setCapability("OS", os);
+        cap.setCapability("os_version", os_version);
+
+        if(envName.equalsIgnoreCase("Browserstack")) {
+            driver = new RemoteWebDriver(new URL("http://"+browserStackUserName+":"+browserStackAccessKey+
+                    "@hub-cloud.browserstack.com/wd/hub"), cap);
+        }
+        else if (envName.equalsIgnoreCase("Saucelabs")) {
+            driver = new RemoteWebDriver(new URL("http://"+sauceLabsUserName+":"+sauceLabsAccessKey+
+                    "@ondemand.saucelabs.com:80/wd/hub"), cap);
+        }
+
+        return driver;
+    }
+
 
     @AfterMethod
     public void cleanUp() {
@@ -143,6 +223,9 @@ public class CommonAPI {
         return text;
     }
 
+
+
+
     public List<WebElement> getListOfWebElements(String locator) {
         List<WebElement> elementList = driver.findElements(By.cssSelector(locator));
         return elementList;
@@ -210,5 +293,7 @@ public class CommonAPI {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         boolean element = wait.until(ExpectedConditions.elementToBeSelected(locator));
     }
+
+
 
 }
